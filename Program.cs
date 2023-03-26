d71af36ee5673effd1e131b14ab6052ee7969360
Nikita Isakov <neta782278@yandex.ru>
2023-03-26 16:06:04 +0700

    SDELANO

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿string[] createArray(int size)
+{
+    string[] array=new string[size];
+
+    for(int i=0; i<size; i++)
+    {
+        Console.Write($"Введите {i+1} значение массива: ");
+        array[i]=Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
+int elements(string[] array, int size)
+{
+    int k=0;
+    for(int i=0; i<size; i++)
+    {
+        if(array[i].Length<3)
+        {
+            k++;
+        }
+    }
+    return k;
+}
+
+string[] newArray(string[] array, int num)
+{
+    string[] mas=new string[num];
+    int k=0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i].Length<=2)
+        {
+            mas[k]=array[i];
+            k++;
+        }
+    }
+    return mas;
+}
+
+void printArray(string[] array)
+{
+    for(int i=0; i<array.Length; i++)   // последняя фигня это как раз размер массива
+    Console.Write(array[i]+" ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Введите размерность массива: ");
+int razmer=Convert.ToInt16(Console.ReadLine());
+string[] masive=createArray(razmer);
+int newRazmer=elements(masive, razmer);
+string[] newMasive=newArray(masive, newRazmer);
+Console.WriteLine("Новый массив, где размерность элементов меньше 2");
+printArray(newMasive);
